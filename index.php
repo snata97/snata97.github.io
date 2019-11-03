f2248a8aa18f738cc6735ca65dca52884022c237
Tasha <39377219+snata97@users.noreply.github.com>
2019-11-03 11:34:37 +0300

    second

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Weather</title>
+    <link rel="stylesheet" href="/assets/css/style.css">  
+</head>
+<body>
+    <form autocomplete="off">
+        <div class="autocomplete" style="width:300px;">
+            <input id="myInput" type="text" name="myCity" placeholder="Город">
+        </div>
+        <input type="submit">
+        </label>
+    </form>
+    
+    
+<!-- JS -->
+    <script src="assets/js/app.js" ></script>
+</body>
+</html>
