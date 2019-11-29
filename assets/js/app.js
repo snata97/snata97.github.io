db0f399404ee1566f61556eac9a88017ede7f9c8
Tasha <39377219+snata97@users.noreply.github.com>
2019-11-29 10:24:17 +0300

    sum

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,50 @@
-document.getElementById("inputCity").addEventListener('input', update);
+document.getElementById("inputCity").addEventListener("input", update);
+document.querySelectorAll('.ulCities__item').forEach(li => {
+    li.addEventListener("click", getCity);
+});
 ymaps.ready(init);
 let myMap;
 
-function getCities(cityValue) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address");
-    let data = JSON.stringify(
-    {
-        query : cityValue,
-        count : '3',
-        from_bound: { "value": "city" },
-        to_bound: { "value": "city" }
-    });
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Authorization", "Token c75c303d3b5f500995a3a79c44ee7ba394f195f8");
-    xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-    xhr.onerror = () => reject(xhr.statusText);
-    xhr.send(data);
+function getDataFromApi(method, url, headers, data){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        if(method=="POST"){
+            for(let headerKey in headers){
+                xhr.setRequestHeader(headerKey, headers[headerKey]);
+            }
+            xhr.send(data);
+        }
+        else{
+            xhr.send();
+        }
+        xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+        xhr.onerror = () => reject(xhr.statusText);
   });
 }
 
+function getCities(cityValue) {
+        let data = JSON.stringify(
+        {
+            query : cityValue,
+            count : "3",
+            from_bound: { "value": "city" },
+            to_bound: { "value": "city" }
+        });
+        let headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Token c75c303d3b5f500995a3a79c44ee7ba394f195f8",
+        };
+        return getDataFromApi("POST", "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address", headers, data);
+}
+
 function getCoords(cityValue) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://geocode-maps.yandex.ru/1.x?apikey=fe6ff201-9007-40b4-8641-4ee03d369364&geocode="+cityValue+"&format=json");
-        xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send();
-    });
+    return getDataFromApi("GET", "https://geocode-maps.yandex.ru/1.x?apikey=fe6ff201-9007-40b4-8641-4ee03d369364&geocode="+cityValue+"&format=json", undefined, undefined);
 }
 
 function getWeather(point){
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+point[1]+"&lon="+point[0]+"&appid=623d727b9cee4746bf0c777c2743f7dd");
-        xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send();
-    });
+    return getDataFromApi("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+point[1]+"&lon="+point[0]+"&appid=623d727b9cee4746bf0c777c2743f7dd", undefined, undefined);
 }
 
 function update({
@@ -49,7 +54,28 @@ function update({
 }) {
     getCities(value).then(
         response => displayResult(response.suggestions),
-        error => console.log(`Rejected: ${error}`)
+        error => console.log("Rejected: ${error}" )
+    );
+}
+
+function getCity(){
+    let city = this.innerHTML;
+    let coords;
+    document.getElementById("inputCity").value = city;
+    hideCities();
+    getCoords(city).then(
+        response => {
+            coords = response.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ', 2);// небезопасно?
+            return getWeather(coords);
+        },
+        error => console.log("Rejected: ${error}"),
+    ).then(
+        response =>{
+            let tempreture = getCelsius(response.main.temp);
+            outputWeather(tempreture);
+            return setTempretureOnMap(coords, tempreture);
+        },
+        error => console.log("Rejected: ${error}"),
     );
 }
 
@@ -61,7 +87,6 @@ function displayResult(cities){
 function hideCities() {
     let listCities = document.querySelectorAll('#ulCities li');
     for(let i = 0; i < 3; i++){
-        listCities[i].classList.add('display_none');// этого можно вообще избежать, если написать подходящий стиль. посмотри псевдо-классы
         listCities[i].innerHTML="";
     }
 }
@@ -72,47 +97,22 @@ function addCities(cities) {
     let cityData;
     for(let i = 0; i < 3 && i < countsCities; i++){
         cityData = cities[i];
-        let region_with_type = cityData.data.region_with_type;
+        let regionWithType = cityData.data.region_with_type;
         let city = cityData.data.city;
-        let city_name_full = cityData.value;
+        let cityNameFull = cityData.value;
         let cityForLi;
-        if(city_name_full.toUpperCase() === region_with_type.toUpperCase()){
+        if(cityNameFull.toUpperCase() === regionWithType.toUpperCase()){
             cityForLi = city;
         }
         else {
-            cityForLi = city + ", " + region_with_type;
+            cityForLi = city + ", " + regionWithType;
         }
         listCities[i].innerHTML = cityForLi;
-        listCities[i].classList.remove("display_none");
     }
-}
-
-function getCity(valueCity){
-    let city = valueCity.innerHTML;
-    document.getElementById("inputCity").value = city;
-    hideCities();
-    let coords, tempreture;
-    getCoords(city).then(
-        response => {
-            coords = response.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ', 2);// небезопасно?
-            return getWeather(coords);
-        },
-        error => console.log(`Rejected: ${error}`),
-    ).then(
-        response =>{
-            tempreture = getCelsius(response.main.temp);
-            outputWeather(tempreture);
-            return setTempretureOnMap(coords, tempreture);
-        },
-        error => console.log(`Rejected: ${error}`),
-    );
 }
 
 function getCelsius(tempInKelvin){
     const T0=273.15;
-    const weather = document.getElementById("weather");
-    const nameCity = document.getElementById("nameCity");
-    const temperature = document.getElementById("temperature");
     let tempInCelsius = (tempInKelvin - T0).toFixed(1);
     return tempInCelsius;
 }
@@ -137,14 +137,16 @@ function init () {
             iconContent: "5",
         }
     }, {
-        preset: 'islands#blueStretchyIcon'
+        preset: "islands#blueStretchyIcon"
     }),
     myMap.geoObjects.add(myGeoObject);
 }
 
 function setTempretureOnMap(coords, tempreture){
-    myMap.setCenter([coords[1],coords[0]]);
+    let lat = coords[1];
+    let lon = coords[0]
+    myMap.setCenter([lat,lon]);
     let mapIcon = myMap.geoObjects.get(0);
-    mapIcon.geometry.setCoordinates([coords[1], coords[0]]);
+    mapIcon.geometry.setCoordinates([lat, lon]);
     mapIcon.properties.set("iconContent",tempreture+" °C");
 }
